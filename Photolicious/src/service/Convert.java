bdf73948b48de623710bad119decb594cb0fbc01
HOME <abhinay_agarwal@infosys.com>
2013-08-17 11:34:02 +0530

    Checkin convert.java

---
+++ Photolicious/src/service/Convert.java
@@ -0,0 +1,66 @@
+package service;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Convert implements Runnable {
+
+	String inputFolderPath;
+	String waterMarkImagePath;
+	String outputImagePath;
+	ImageOverlay imageOverlay;
+	ResizePic resizePic;
+	List<String> currentFolderList;
+	List<String> convertedFilesList;
+	
+	public Convert(String inputFolderPath, String waterMarkImagePath, String outputImagePath)
+	{
+		this.inputFolderPath = inputFolderPath;
+		this.waterMarkImagePath = waterMarkImagePath;
+		this.outputImagePath = inputFolderPath;
+		imageOverlay = new ImageOverlay();
+		resizePic = new ResizePic();
+		currentFolderList = new ArrayList<String>();
+		convertedFilesList = new ArrayList<String>();
+	}
+	@Override
+	public void run() {
+		try{
+		while(!Thread.interrupted())
+		{
+			File inputFolder = new File(this.inputFolderPath);
+      	  	File watermark = new File(this.waterMarkImagePath);
+      	  	BufferedImage watermarkBuffer = imageOverlay.readImage(watermark.getPath());
+            File[] listOfFiles = inputFolder.listFiles();
+            if(listOfFiles == null) return;  // Added condition check
+            System.out.println("Start Time"+ new Date());
+         
+            if(listOfFiles.length != convertedFilesList.size())
+        	{
+            	for (File file : listOfFiles) {
+            		if(!convertedFilesList.contains(file.getName()))
+            		{
+		          	  System.out.println(file.getPath());
+		          	  BufferedImage image = imageOverlay.readImage(file.getPath());
+		          	  BufferedImage resizedImage = resizePic.scaleImage(image, watermarkBuffer.getWidth(), watermarkBuffer.getHeight());
+		          	  BufferedImage finalImage = imageOverlay.overlayImages(resizedImage, watermarkBuffer);
+		          	  imageOverlay.writeImage(finalImage, outputImagePath+"\\"+file.getName(), "jpeg");
+		          	  convertedFilesList.add(file.getName());
+            		}
+            	}
+            	System.out.println("End Time"+ new Date());
+            }
+            Thread.sleep(5000);
+			}
+		}catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+
+		}
+		
+	}
+
+}
